d30a678e776e0a3d882b7e9a2ac681380d8a09e7
fvillena <villenafabian@gmail.com>
2019-07-09 21:01:43 -0400

    working page to display categories

---
+++ category.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "api.php",
+        data: {
+          'action': 'viewCategory',
+          'id': id
+        },
+        type: 'GET',
+        success: function( result ) {
+          $( "#category" ).html( result.name );
+          $( "#description" ).html( result.description );
+          document.title = result.name;
+          $("#concepts").html('');
+          $.each(result.terms, function (index, value) {
+            $("#concepts").append("<li>" + "<a href=term.php?id=" + value.id + ">" + value.concept + "</a>" + "</li>");
+        });
+        }
+      });
+});
---
+++ category.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Categoría</title>
+</head>
+<body>
+<div class='container-fluid'>
+<?php include('header.php'); ?>
+<form class="form-inline" action="search.php" method="get">
+
+  <input class='form-control' type="text" name="query"> 
+
+  <input class='btn btn-primary' type="submit" value="Buscar">
+
+</form>
+<hr>
+    <h2 id=category>...</h2>
+    <p id=description>...</p>
+    <h3>Conceptos</h3>
+    <ul id=concepts>...</ul>
+    <script src="jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+    var id = "<?php echo $_GET["id"]; ?>"
+  </script>
+    <script type="text/javascript" src="category.js"></script>
+    <?php include('footer.php'); ?>
+    </div>
+</body>
+</html>
