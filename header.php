3a2d0f51aa7a631ab9f35423c3145b911bf48b7a
fvillena <villenafabian@gmail.com>
2019-07-13 11:58:08 -0400

    changing header to a navbar

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
-<h1 class='text-center'><a href='index.php'>Glosario de Odontología</a></h1>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<nav class="navbar ">
+
+    <a class="navbar-brand" href="index.php"><img src="https://www.unab.cl/wp-content/themes/unab-base/img/logos/logo-header.png" height="30" alt=""> Información Odontológica</a>
+  <form class="form-inline" action="search.php" method="get">
+    <input class="form-control mr-sm-2" type="search" name="query" placeholder="caries" aria-label="Search">
+    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Buscar</button>
+  </form>
+</nav>    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/b7c0bed79a.js"></script>
