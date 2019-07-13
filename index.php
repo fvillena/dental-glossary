2f2c6103256d59b1cd3b426baa8e99764c047fbc
fvillena <villenafabian@gmail.com>
2019-07-13 11:58:42 -0400

    adding template for site information and specialties

--- index.php
+++ index.php
@@ -7,16 +7,58 @@
     <title>Glosario Odontológico</title>
 </head>
 <body>
-<div class='container-fluid'>
+<div class='container'>
     <?php include('header.php'); ?>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum nisi, hic dicta quisquam sit soluta porro exercitationem excepturi in animi minima cumque consequatur quae suscipit magnam obcaecati cupiditate accusantium molestias?</p>
-    <form class="form-inline" action="search.php" method="get">
+    
 
-  <input class='form-control' type="text" name="query"> 
+    
+<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 
-  <input class='btn btn-primary' type="submit" value="Buscar">
+</div>
+<div class="card-deck">
+  <div class="card text-white bg-info">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
 
-</form>
+    <div class="card-body">
+      <h5 class="card-title">Rehabilitación Oral</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer bg-transparent border-success text-center">
+    <a href="category.php?id=2" class="btn btn-primary">Ver Más</a>
+    </div>
+
+  </div>
+  <div class="card text-white bg-info">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
+
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      
+    </div>
+    <div class="card-footer bg-transparent border-success text-center">
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+
+
+  </div>
+  <div class="card text-white bg-info">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
+
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer bg-transparent border-success text-center">
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+
+  </div>
+</div>
 <?php include('footer.php'); ?>
 </div>
 </body>
