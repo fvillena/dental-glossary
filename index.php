767e9f2b21b6c1a6661f2cdbe8d3f507e31b5911
Fabián Villena <villenafabian@gmail.com>
2019-07-29 02:51:54 -0400

    updated categories and corrected greeting typo

--- index.php
+++ index.php
@@ -13,13 +13,14 @@
 
     
 <div class="jumbotron">
-  <h1 class="display-4">Bienvenio(a)!</h1>
+  <h1 class="display-4">Bienvenido(a)!</h1>
   <p class="lead">Esta página fue diseñada por alumnas de la carrera de odontología de la Universidad Andrés Bello y tiene como finalidad entregar información odontológica de fácil comprensión a la comunidad. Esperamos que sea de gran ayuda.</p>
   <hr class="my-4">
   <p>Acá abajo encontrarás los distintos temas disponibles.</p>
 
 </div>
-<div class="card-deck">
+<div class="card-columns">
+
   <div class="card text-white bg-info">
   <img src="https://www.clinicaeverest.cl/wp-content/uploads/2017/10/Incrustacio%CC%81n-dental.png" class="card-img-top" alt="Rehabilitación Oral">
 
@@ -33,32 +34,46 @@
 
   </div>
   <div class="card text-white bg-info">
-  <!-- <img src="..." class="card-img-top" alt="..."> -->
+  <img src="https://clinicaenrile.es/wp-content/uploads/2019/01/periodoncia3.jpg" class="card-img-top" alt="Periodoncia">
 
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">Periodoncia</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       
     </div>
     <div class="card-footer bg-transparent border-success text-center">
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="category.php?id=3" class="btn btn-primary">Ver Más</a>
     </div>
 
 
   </div>
   <div class="card text-white bg-info">
-  <!-- <img src="..." class="card-img-top" alt="..."> -->
+  <img src="http://www.lastraclinicadental.com/wp-content/uploads/2014/11/servicios-endodoncia.jpg" class="card-img-top" alt="Endodoncia">
 
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">Endodoncia</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
     </div>
     <div class="card-footer bg-transparent border-success text-center">
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="category.php?id=4" class="btn btn-primary">Ver Más</a>
     </div>
 
   </div>
+  <div class="card text-white bg-info">
+  <img src="http://www.ortodonciafuncional.es/wp-content/uploads/2018/12/odontopediatria.jpg" class="card-img-top" alt="Odontopediatría">
+
+    <div class="card-body">
+      <h5 class="card-title">Odontopediatría</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer bg-transparent border-success text-center">
+    <a href="category.php?id=5" class="btn btn-primary">Ver Más</a>
+    </div>
+
+  </div>
+  
 </div>
+
 <?php include('footer.php'); ?>
 </div>
 </body>
