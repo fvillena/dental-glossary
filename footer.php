1793daa4089fc205cd3d857e20b7cbfed88034d3
Fabián Villena <villenafabian@gmail.com>
2019-07-29 23:34:36 -0400

    linkedin for felipe

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <hr>
-Desarrollo Web Por: <a href="https://fabianvillena.cl">Fabián Villena</a> & <a href="https://www.facebook.com/fhcavilla">Felipe Cáceres</a> - Informática Médica
+Desarrollo Web Por: <a href="https://fabianvillena.cl">Fabián Villena</a> & <a href="https://www.linkedin.com/in/felipe-c%C3%A1ceres-villanueva-968711170/">Felipe Cáceres</a> - Informática Médica
 <br><br>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
