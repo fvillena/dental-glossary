35a79fd39fbac434190a13c646f89a508999ced7
Fabián Villena <villenafabian@gmail.com>
2020-05-10 16:26:58 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,14 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/b7c0bed79a.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166080742-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-166080742-1');
+</script>
 <style>
 .navbar {background-color: #FFF;}
 #authors {background-color: #FFF;
